14c774ec02a59dcb3f79554b5c6a4cee31f9dcb0
Ramy Khorshed <ramykhorshed@gmail.com>
2017-09-13 16:57:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
 # Add  code here!
+def divisible?(number,divisor)
+  if number%divisor == 0
+    true
+  else
+    false
+  end
+end
+
+def prime?(number)
+  if number < 2
+    false
+  else
+    outcomes =[]
+    for counter in 2...number
+      outcomes << divisible?(number,counter)
+    end
+    if outcomes.include?(true)
+        puts "This is not a prime number"
+        false
+      else
+        puts "This is a prime number"
+        true
+    end
+  end
+end
